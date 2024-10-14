0cad93528c7a99388b30b571b107190f914ad27e
Emicora <e.coratolo@alumno.um.edu.ar>
2024-10-14 17:24:59 -0300

    springboot

---
+++ src/main/java/com/example/inicial1/entities/Persona.java
@@ -0,0 +1,28 @@
+package org.example.persona.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "persona")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+
+public class Persona extends Base {
+
+
+
+    @Column(name = "nombre")
+    private String nombre;
+    @Column(name = "apellido")
+    private String apellido;
+    @Column(name = "dni")
+    private int dni;
+}
---
+++ src/main/java/com/example/inicial1/repositories/PersonaRepository.java
@@ -0,0 +1,8 @@
+package org.example.persona.repositories;
+
+import org.example.persona.entities.Persona;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonaRepository extends BaseRepository<Persona, Long> {
+}
